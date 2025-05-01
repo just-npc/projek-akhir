ad2e861d304a26bb36eee165d6c938a5619fd56e
Karyawan magang poludnitsa#1 <158252972+Dhoo-01@users.noreply.github.com>
2025-05-01 15:33:15 +0800

    Update orders.js

--- final-assessment/optional-07-real-world-scenario/orders.js
+++ final-assessment/optional-07-real-world-scenario/orders.js
@@ -5,18 +5,49 @@ function generateUniqueId() {
 
 
 // TODO: buatlah variabel yang menampung data orders
-let orders;
+let orders = [];
 
 // TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+function addOrder(customerName, items) {
+  const total = items.reduce((acc, item) => acc + item.price, 0);
+  
+  orders.push({
+    id: generateUniqueId(),
+    customerName: customerName,
+    items: JSON.stringify(items),
+    totalPrice: total,
+    status: "menunggu",
+  });
+}
 
 // TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+function updateOrderStatus(orderId, status) {
+  const order = orders.find(ord => ord.id === orderId);
+  
+  if (order) {
+    order.status = status;
+    console.log("succcess");
+  } else {
+    console.log("there's no id like that!");
+  }
+}
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  let total = 0;
+  
+  for (i = 0; i < orders.length; i++) {
+    if (orders[i].status === "Selesai") {
+      total += orders[i].totalPrice;
+    }
+  }
+  
+ return total;
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  orders = orders.filter(order => order.id !== id);
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
